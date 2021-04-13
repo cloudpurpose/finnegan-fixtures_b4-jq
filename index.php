54ddc4cd9d409221c077917344b6a5aee96b8904
dbarbero3 <dbarbero3@gatech.edu>
2021-04-13 12:43:37 -0400

    Mock-up ready for manager review

---
+++ index.php
@@ -0,0 +1,320 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="title" content="Bookings | Office of Scholarships and Financial Aid | Georgia Tech">
+    <meta name="description" content="Themed page for GT portal concept with Bootstrap 4 and JQuery.">
+    <meta name="keywords" content="Georgia Tech, gatech">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="author" content="David Barbero">
+    <link rel="apple-touch-icon" href="../_assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="../_assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="../_assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="../_assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="../_assets/img/favicons/safari-pinned-tab.svg" color="#B3A369">
+    <link rel="icon" href="../_assets/img/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#B3A369">
+    <meta name="theme-color" content="#B3A369">
+    <meta name="DC.title" content="Atlanta" />
+    <meta name="geo.region" content="US-GA" />
+    <meta name="geo.placename" content="Atlanta" />
+    <meta name="geo.position" content="33.772337;-84.394711" />
+    <meta name="ICBM" content="33.772337, -84.394711" />
+    <title>Bookings | Office of Scholarships and Financial Aid | Georgia Tech</title>
+    <!--<link rel="canonical" href="canonical link goes here">-->
+    <!-- Bootswatch CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Font Aweomse 5 -->
+    <link rel="stylesheet" href="../_assets/fontawesome/css/fontawesome.min.css" media="all">
+    <link rel="stylesheet" href="../_assets/fontawesome/css/duotone.min.css" media="all">
+    <link rel="stylesheet" href="../_assets/fontawesome/css/brands.min.css" media="all">
+    <!-- Google Fonts: Abel, Roboto, Roboto Condensed -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Roboto+Condensed:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <!-- Site CSS -->
+    <link rel="stylesheet" href="../_assets/css/scroll.css" media="all">
+    <link rel="stylesheet" href="../_assets/css/template.css" media="screen">
+    <link rel="stylesheet" href="../_assets/css/screen.css" media="screen">
+    <link rel="stylesheet" href="../_assets/css/print.css" media="print">
+</head>
+
+<body>
+
+    <!-- Header -->
+    <header class="sticky-top">
+        <!-- Topper -->
+        <div class="container-fluid bg-warning topper">
+            <div class="row justify-content-md-center pt-2 pb-2">
+                <div class="col-md-auto">
+                    <p class="mb-0"><a class="text-dark font-weight-bold" href="https://health.gatech.edu/tech-moving-forward#utm_source=osfa&utm_medium=banner" target="_blank">Get the latest Covid-19 health information, operational status updates, and campus guidelines.</a></p>
+                </div>
+            </div>
+        </div>
+        <!-- /.topper -->
+        <!-- Persistent Header -->
+        <nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow persistent-header">
+
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0 p-0 logo" href="https://www.gatech.edu">
+                <span class="hidden">Georgia Tech</span>
+                <div class="logo"></div>
+            </a>
+
+            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <ul class="navbar-nav mr-auto">
+                <span class="nav-item navbar-text two-line text-fancy">
+                    Office of Scholarships and Financial Aid
+                    <div class="bottom-line">Bookings</div>
+                </span>
+            </ul>
+
+            <ul class="navbar-nav px-3 inner-right">
+                <li class="nav-item">
+                    <span class="navbar-text">
+                        Signed in as dbarbero3
+                    </span>
+                </li>
+                <li class="nav-item text-nowrap">
+                    <a class="nav-link" href="#">Sign out</a>
+                </li>
+            </ul>
+
+        </nav>
+        <!-- /.persistent-header -->
+    </header>
+    <!-- /header -->
+
+    <!-- Wrapper -->
+    <div class="container-fluid wrapper">
+        <div class="row">
+
+            <!-- Secondary Area -->
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar secondary-area collapse">
+                <div class="sidebar-sticky pt-3">
+                    <div class="inner">
+                        <!-- Sidebar Menu -->
+                        <div class="sidebar-menu">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link active" href="../"><i class="fad fa-home fa-fw mr-2"></i>Welcome <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../booking-page"><i class="fad fa-calendar-alt fa-fw mr-2"></i>Booking Page</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.sidebar-menu -->
+                        <!-- Sidebar Menu -->
+                        <div class="sidebar-menu">
+                            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                                <span>Admin Menu</span>
+                            </h6>
+                            <ul class="nav flex-column mb-2">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="../admin"><i class="fab fa-microsoft fa-fw mr-2"></i>Microsoft 365</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.sidebar-menu -->
+                        <!-- Sidebar Menu -->
+                        <div class="sidebar-menu">
+                            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+                                <span>Helpful Link</span>
+                            </h6>
+                            <ul class="nav flex-column mb-2">
+                            <li class="nav-item">
+                                    <a class="nav-link" href="https://finaid.gatech.edu" target="_blank"><i class="fad fa-browser fa-fw mr-2"></i>OSFA Website</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.sidebar-menu -->
+                    </div>
+                </div>
+            </nav>
+            <!-- /.secondary-area -->
+
+            <!-- Main -->
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+
+                <!-- Primary Area -->
+                <div class="container-fluid primary-area">
+                    <div class="row">
+                        <div class="col-12 pl-0 pr-0">
+
+                            <!-- Title Area -->
+                            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom title-area">
+                                <h1 class="h2 page-title">Welcome</h1>
+                            </div>
+                            <!-- /.title-area -->
+
+                            <!-- Content Area -->
+                            <section class="content-area">
+                                <article>
+
+                                    <p>This is where tutorial content will go.</p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <hr/>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h3>About Bookings</h3>
+
+                                    <p>To book an online consulation or an in-person consultation with a representative of the Office of Scholarships and Financial Aid, use the booking page.</p>
+
+                                    <p><a class="btn btn-outline-secondary" href="../booking-page">Go to the booking page</a></p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <hr/>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h3>About the Booking Page</h3>
+
+                                    <p>Lorem ipsum</p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h4>Select service</h4>
+
+                                    <p>Lorem ipsum</p>
+
+                                    <ul>
+                                        <li><strong>Online Consultation</strong>: Lorem ipsum</li>
+                                        <li><strong>In-Person Consultation</strong>: Lorem ipsum</li>
+                                    </ul>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h4>Select time</h4>
+
+                                    <p>Lorem ipsum</p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h4>Add your details</h4>
+
+                                    <p>Lorem ipsum</p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h4>Provide additional information</h4>
+
+                                    <p>Lorem ipsum</p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h4>The "Book" button</h4>
+
+                                    <p>Lorem ipsum</p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h4>Automated email messages</h4>
+
+                                    <p>Lorem ipsum</p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                    <h4>After your consultation is scheduled.</h4>
+
+                                    <p>Lorem ipsum</p>
+
+                                    <div class="row w-100 mb-3"></div>
+
+                                </article>
+                            </section>
+                            <!-- /.content-area -->
+                        </div>
+                    </div>
+                </div>
+                <!-- /.primary-area -->
+
+                <!-- Footer -->
+                <footer>
+                    <!-- Accent Footer -->
+                    <div class="container-fluid accent-footer">
+                        <div class="row">
+                            <div class="col">
+                                <p class="mb-0">&nbsp;</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.accent-footer -->
+
+                    <!-- Page Footer -->
+                    <div class="container-fluid bg-secondary pt-3 pb-3 pl-5 pr-5 page-footer">
+                        <div class="row">
+                            <div class="col-sm-12 col-md-4 col-lg-3 pt-1">
+                                <img class="img-fluid p-0 mb-3" style="max-width: 220px;"
+                                    src="https://registrar-dev.gatech.edu/_includes/themes/dev/assets/img/pics/creating_the_next_black.svg"
+                                    alt="Creating the Next" loading="lazy">
+                                <address class="left-bordered">
+                                    <strong>Georgia Institute of Technology</strong><br>
+                                    North Avenue, Atlanta, GA 30332<br>
+                                    404.894.2000
+                                </address>
+                            </div>
+                            <div class="col-sm-12 col-md-8 col-lg-9 pt-1">
+                                <div class="row">
+                                    <div class="col-sm-4">
+                                        <div class="left-bordered">
+                                            <p><a href="https://www.directory.gatech.edu/" target="_blank">Directory</a></p>
+                                            <p><a href="http://www.gatech.edu/emergency/" target="_blank">Emergency Information</a></p>
+                                            <p><a href="http://www.gatech.edu/legal/" target="_blank">Legal & Privacy Information</a></p>
+                                            <p><a href="https://gbi.georgia.gov/human-trafficking-notice" target="_blank">Human Trafficking Notice</a></p>
+                                            <p><a href="https://titleix.gatech.edu/" target="_blank">Title IX/Sexual Misconduct</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <div class="left-bordered">
+                                            <p><a href="http://www.gatech.edu/accessibility/" target="_blank">Accessibility</a></p>
+                                            <p><a href="http://www.gatech.edu/accountability/" target="_blank">Accountablility</a></p>
+                                            <p><a href="https://www.gatech.edu/accreditation/" target="_blank">Accreditation</a></p>
+                                            <p><a href="http://www.careers.gatech.edu/" target="_blank">Employment</a></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-4">
+                                        <img class="img-fluid float-right p-0 mb-3" style="max-width: 140px;" src="../_assets/img/logos/gt-logo.svg" alt="Creating the Next" loading="lazy">
+                                        <p class="float-right copyr">&#169;&nbsp;Georgia Institute of Technology</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.page-footer -->
+                    <!-- Back to Top -->
+                    <div class="scroll-top is-hidden"><button onclick="scrollToTop()" class="scroll-btn"><i class="fad fa-angle-up fa-3x"></i></button></div>
+                    <!-- /.back-to-top -->
+                </footer>
+                <!-- /footer -->
+
+            </main>
+            <!-- /main -->
+
+        </div>
+    </div>
+    <!-- /.wrapper -->
+
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <!-- Popper JS CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS CDN -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- Custom Vanilla JavaScript -->
+    <script src="../_assets/js/vanilla/scripts.js"></script>
+    <script src="../_assets/js/vanilla/scroll.js"></script>
+    <!-- Custom JQuery JavaScript -->
+    <script src="../_assets/js/jquery/scripts.js"></script>
+
+</body>
+
+</html>
